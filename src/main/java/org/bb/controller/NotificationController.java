20b4b0614aa40a71ec291bc45ff06b05e48864b0
Igor Stojanovski <igor.stojanovski@endava.com>
2016-06-24 20:23:27 +0200

    Sending hardcoded push notifications.

--- src/main/java/org/bb/controller/NotificationController.java
+++ src/main/java/org/bb/controller/NotificationController.java
@@ -1,5 +1,11 @@
 package org.bb.controller;
 
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Result;
+import com.google.android.gcm.server.Sender;
+import org.bb.entity.Token;
+import org.bb.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/random/send/")
 public class NotificationController {
 
-    @RequestMapping(value = "/{param}", method = RequestMethod.POST)
-    public ResponseEntity<String> getMessage(@PathVariable("param") String param) {
+    @Autowired
+    TokenService tokenService;
 
-        return new ResponseEntity<>(param, HttpStatus.OK);
+    @RequestMapping(value = "/{param}", method = RequestMethod.POST)
+    public ResponseEntity<String> sendMessage(@PathVariable("param") String param) throws IOException {
+
+        Token token = tokenService.retrieveToken((long) 1);
+
+        Sender sender = new Sender("AIzaSyDKCQ7aOEVJ4F5EF9QBRc_F3xDSC8DGC8M");
+        Message message = new Message.Builder()
+                .addData("message", "this is the message")
+                .addData("title", "15% off in H&M")
+                .addData("route", "")
+                .addData("other-parameter", "some value")
+                .build();
+        Result result = sender.send(message, token.getToken(), 3);
+
+        return new ResponseEntity<>(result.getMessageId(), HttpStatus.OK);
     }
 }
